881f97a1bd2d9eca54f690b595e56151d54d3f0a
Dilla <122979935+Dillaayh@users.noreply.github.com>
2024-12-11 13:57:39 +0800

    Update pakanRoutes.js

--- src/routes/pakanRoutes.js
+++ src/routes/pakanRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createPakan, getPakanById, updatePakan, deletePakan } from '../controllers/pakanController.js';
+import { createPakan, getPakanById, updatePakan, deletePakan, searchPakanByName, getTotalStokPakan } from '../controllers/pakanController.js';
 
 const router = express.Router();
 
@@ -14,5 +14,11 @@ router.put('/:idPakan', updatePakan);
 
 // Route untuk menghapus data pakan berdasarkan ID
 router.delete('/:idPakan', deletePakan);
+
+// Route untuk mencari pakan berdasarkan jenis pakan
+router.get('/search', searchPakanByName);
+
+// Define the endpoint
+router.get('/total-stok-pakan', getTotalStokPakan);
 
 export default router;
