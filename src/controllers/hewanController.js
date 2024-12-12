5d527923aef511fc1a6721cf6758fa68f85b12f7
Dilla <122979935+Dillaayh@users.noreply.github.com>
2024-12-12 12:56:32 +0800

    Update hewanController.js

--- src/controllers/hewanController.js
+++ src/controllers/hewanController.js
@@ -34,3 +34,61 @@ export const deleteHewan = (req, res) => {
     res.status(200).json({ message: 'Hewan berhasil dihapus' });
   });
 };
+
+// Fungsi untuk mencari hewan berdasarkan spesies
+export const searchHewanByName = (req, res) => {
+  const { spesies } = req.query;
+
+  if (!spesies) {
+    return res.status(400).json({ message: 'Parameter spesies harus diberikan' });
+  }
+
+  db.query('CALL SearchHewanByName(?)', [spesies], (err, results) => {
+    if (err) return res.status(500).json({ error: err.message });
+    res.status(200).json(results[0]);
+  });
+};
+
+// Controller untuk menghitung jumlah hewan berdasarkan status kesehatan
+export const getJumlahHewanByStatusKesehatan = (req, res) => {
+  const { statusKesehatan } = req.body; // Ambil parameter dari request body
+  console.log('Status Kesehatan:', statusKesehatan); // Log statusKesehatan untuk memastikan nilai yang diterima
+
+  // Panggil prosedur dengan parameter input dan output
+  db.query("CALL get_jumlah_hewan_by_status_kesehatan(?, @jumlahHewan);", [statusKesehatan], (err, results) => {
+    if (err) {
+      console.error("Error menjalankan prosedur:", err);
+      return res.status(500).json({
+        status: "error",
+        message: "Terjadi kesalahan pada server",
+      });
+    }
+
+    console.log("Results from first query:", results); // Log hasil dari query pertama
+
+    // Ambil hasil output dari prosedur
+    db.query("SELECT @jumlahHewan AS jumlahHewan;", (err, result) => {
+      if (err) {
+        console.error("Error mengambil hasil output:", err);
+        return res.status(500).json({
+          status: "error",
+          message: "Terjadi kesalahan pada server",
+        });
+      }
+
+      console.log("Results from second query:", result); // Log hasil dari query kedua
+
+      if (result.length > 0) {
+        res.json({
+          status: "success",
+          jumlahHewan: result[0].jumlahHewan,
+        });
+      } else {
+        res.status(404).json({
+          status: "error",
+          message: "Jumlah hewan tidak ditemukan",
+        });
+      }
+    });
+  });
+};
