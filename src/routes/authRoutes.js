d1e9cafe4bc729727f1c0e2f4360cff789893c8a
Dilla <122979935+Dillaayh@users.noreply.github.com>
2024-12-12 12:58:22 +0800

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,13 @@
-// routes/authRoutes.js
-import express, { request, response } from "express"
-import { loginUser } from '../controllers/authController.js';
+import express from 'express';
+import { registerUser, loginUser } from '../controllers/authController.js';
+import { verifyToken } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
+// Route for User Registration
+router.post('/register', registerUser, verifyToken);
+
+// Route for User Login
 router.post('/login', loginUser);
 
 export default router;
